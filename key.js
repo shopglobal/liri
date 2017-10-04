2f2a74bb914ff27cd2e02a84b25e678c351dbf7b
shopglobal <president@worldvaporexpo.com>
2017-10-04 19:19:12 -0400

    added more to README.md

---
+++ key.js
@@ -0,0 +1,11 @@
+//console.log('this is loaded');
+var Twitter = require('twitter');
+
+var twitterKeys = new Twitter({
+  consumer_key: 'drmRnUE4FDYwwO1IrCQBbn5tr',
+  consumer_secret: 'VIo83NEyRPlaSq2ots8Nvcs70eR3uCHliWdDPQktO4s6CzMGM3',
+  access_token_key: '915715824064876544-9guBql4nPusg7v6CtBchnRcTwoeoqKZ',
+  access_token_secret: 'OnP7ATVL5JMoEmA0jBH5jEMyQccG5pQ31fJnZSl4UzxZ9',
+});
+
+module.exports = twitterKeys;
